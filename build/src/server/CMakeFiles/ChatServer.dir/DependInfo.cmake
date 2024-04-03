62a12331e8fcf35372d20eb41c30ecc359636fe2
dwhdxp <dxp20001107@163.com>
2024-04-03 22:47:12 +0800

    first commit

---
+++ build/src/server/CMakeFiles/ChatServer.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dwh/TalkServer/Talk/src/server/ChatServer.cpp" "/home/dwh/TalkServer/Talk/build/src/server/CMakeFiles/ChatServer.dir/ChatServer.cpp.o"
+  "/home/dwh/TalkServer/Talk/src/server/ChatService.cpp" "/home/dwh/TalkServer/Talk/build/src/server/CMakeFiles/ChatServer.dir/ChatService.cpp.o"
+  "/home/dwh/TalkServer/Talk/src/server/main.cpp" "/home/dwh/TalkServer/Talk/build/src/server/CMakeFiles/ChatServer.dir/main.cpp.o"
+  "/home/dwh/TalkServer/Talk/src/server/model/FriendModel.cpp" "/home/dwh/TalkServer/Talk/build/src/server/CMakeFiles/ChatServer.dir/model/FriendModel.cpp.o"
+  "/home/dwh/TalkServer/Talk/src/server/model/GroupModel.cpp" "/home/dwh/TalkServer/Talk/build/src/server/CMakeFiles/ChatServer.dir/model/GroupModel.cpp.o"
+  "/home/dwh/TalkServer/Talk/src/server/model/OfflineMessageModel.cpp" "/home/dwh/TalkServer/Talk/build/src/server/CMakeFiles/ChatServer.dir/model/OfflineMessageModel.cpp.o"
+  "/home/dwh/TalkServer/Talk/src/server/model/UserModel.cpp" "/home/dwh/TalkServer/Talk/build/src/server/CMakeFiles/ChatServer.dir/model/UserModel.cpp.o"
+  "/home/dwh/TalkServer/Talk/src/server/redis/Redis.cpp" "/home/dwh/TalkServer/Talk/build/src/server/CMakeFiles/ChatServer.dir/redis/Redis.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../include/server"
+  "../include/server/db"
+  "../include/server/model"
+  "../include/server/redis"
+  "../thirdparty"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/server/CMakeFiles/ChatServer.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "../../../bin/ChatServer"
+  "../../../bin/ChatServer.pdb"
+  "CMakeFiles/ChatServer.dir/ChatServer.cpp.o"
+  "CMakeFiles/ChatServer.dir/ChatService.cpp.o"
+  "CMakeFiles/ChatServer.dir/main.cpp.o"
+  "CMakeFiles/ChatServer.dir/model/FriendModel.cpp.o"
+  "CMakeFiles/ChatServer.dir/model/GroupModel.cpp.o"
+  "CMakeFiles/ChatServer.dir/model/OfflineMessageModel.cpp.o"
+  "CMakeFiles/ChatServer.dir/model/UserModel.cpp.o"
+  "CMakeFiles/ChatServer.dir/redis/Redis.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChatServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
